2db87932728b7020f01d5ca164c7911e7b6509b7
Kasuteryon <eduardo.castellon66i@std.uni.edu.ni>
2021-10-10 18:33:02 -0600

    Added user recognizion and messages bubbles

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import os
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, send
+from flask import Flask, render_template, request
+from flask_session import Session
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
+from werkzeug.utils import redirect
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+socketio = SocketIO(app, manage_session=False)
 
 FLASK_APP = os.getenv("FLASK_APP")
 
@@ -14,13 +19,34 @@ FLASK_APP = os.getenv("FLASK_APP")
 def index():
     return render_template("index.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == 'POST':
+        request.form.get("userlog")
+        return redirect("/");
+
+    return render_template("login.html")
+
 @socketio.on("message")
 def send_message(data):
     selection = data["selection"]
     dateM = data["dateM"]
+    user = data["user"]
     # print(selection)
-    emit("announce message", {"selection": selection, "dateM": dateM}, broadcast=True)
+    emit("announce message", {"user":user,"selection": selection, "dateM": dateM}, broadcast=True)
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', to=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
+
--- static/js/index.js
+++ static/js/index.js
@@ -4,11 +4,13 @@ function sendMessage(){
     var task =  document.querySelector('#task');
     
     let hoy = new Date();
-    var fecha = hoy.getDate() + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getFullYear() + '-'+ hoy.getHours() + ':' + hoy.getMinutes();
+    //let fecha = hoy.getDate() + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getFullYear() + '-'+ hoy.getHours() + ':' + hoy.getMinutes();
+    let fecha = hoy.getHours() + ':' + hoy.getMinutes();
     console.log(fecha.toString());
+    var user = localStorage.getItem('actualUser');
     
     if (task.value) {
-        socket.emit("message", {'selection': task.value, 'dateM': fecha.toString()});
+        socket.emit("message", {'user':user,'selection': task.value, 'dateM': fecha.toString()});
         task.value = '';
       }
     return false;
@@ -16,11 +18,56 @@ function sendMessage(){
 }
 
 socket.on('announce message', data => {
-    const li = document.createElement('li');
-    li.innerHTML = `Mensaje: ${data.selection} Hora: ${data.dateM}`;
-    document.querySelector('#tasks').append(li);
-    
+    const li = document.createElement('div');
+    const row = document.createElement('div');
+    const colOwn = document.createElement('div');
+    const colOther = document.createElement('div');
+
+    li.classList.add('card');
+    const hora = document.createElement('div');
+    hora.classList.add('card-header')
+    const cuerpo = document.createElement('div');
+    cuerpo.classList.add('card-body');
+    const user = document.createElement('h5');
+    user.classList.add('card-title');
+    const message = document.createElement('p');
+    message.classList.add('card-text');
+
+    cuerpo.append(user, message);
+    li.append(hora, cuerpo);
+
+    hora.innerHTML = `Hora: ${data.dateM}`;
+    user.innerHTML = `Por: ${data.user}`;
+    message.innerHTML = `${data.selection}`;
+
+    row.classList.add('row');
+    colOwn.classList.add('col-6');
+    colOther.classList.add('col-6');
+
+    if (data.user == localStorage.getItem('actualUser')){
+        document.querySelector('#tasks').append(row);
+        li.classList.add('bg-success');
+        li.classList.add('text-white');
+        row.append(colOther);
+        row.append(colOwn);
+        colOwn.append(li);
+    }else{
+        document.querySelector('#tasks').append(row);
+        li.classList.add('bg-secondary');
+        li.classList.add('text-white');
+        row.append(colOther);
+        row.append(colOwn);
+        colOther.append(li);
+    }
+    window.scrollTo(0, document.body.scrollHeight);
 }); 
+
+function actualUser(){
+    actualUser = document.getElementById('logemail').value;
+
+    localStorage.setItem('actualUser', actualUser);
+
+}
 
 
 
